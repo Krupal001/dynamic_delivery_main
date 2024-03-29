85b6f485a925e2788329f2d07ebda790bc607d01
krupal001 <krupal24x7@gmail.com>
2024-03-29 16:23:43 +0530

    "test commit"

--- dynamic_delivery/lib/src/features/authentication/screens/notification/message_screen.dart
+++ dynamic_delivery/lib/src/features/authentication/screens/notification/message_screen.dart
@@ -7,7 +7,6 @@ import 'package:get/get.dart';
 
 class MessageScreen extends StatefulWidget {
   const MessageScreen({super.key});
-
   @override
   State<MessageScreen> createState() => _MessageScreenState();
 }
@@ -22,7 +21,6 @@ class _MessageScreenState extends State<MessageScreen> {
   String parcelQTY = '';
   String description = '';
   bool isLoading = true;
-
 
   @override
   void initState() {
@@ -44,20 +42,18 @@ class _MessageScreenState extends State<MessageScreen> {
         String? merchantId = mostRecentDocument?['Merchant_ID'];
         String? parcelId = mostRecentDocument?.id;
 
-
         print("-------$parcelId");
         print(mostRecentDocument?.data());
 
         CollectionReference merchants =
-        FirebaseFirestore.instance.collection('Merchants');
+            FirebaseFirestore.instance.collection('Merchants');
         DocumentSnapshot merchantSnapshot =
-        await merchants.doc(merchantId).get();
+            await merchants.doc(merchantId).get();
         CollectionReference subCollection =
-        merchants.doc(merchantId).collection('Firm-Address');
+            merchants.doc(merchantId).collection('Firm-Address');
         QuerySnapshot subCollectionSnapshot = await subCollection.get();
 
-        DocumentSnapshot? subSnapshot =
-        subCollectionSnapshot.docs.isNotEmpty
+        DocumentSnapshot? subSnapshot = subCollectionSnapshot.docs.isNotEmpty
             ? subCollectionSnapshot.docs[0]
             : null;
 
@@ -88,11 +84,16 @@ class _MessageScreenState extends State<MessageScreen> {
 
   void updateFirestore() async {
     try {
-      QuerySnapshot agentsQuery = await FirebaseFirestore.instance.collection("Delivery-agents")
-          .where("Email", isEqualTo: user?.email!).get();
+      QuerySnapshot agentsQuery = await FirebaseFirestore.instance
+          .collection("Delivery-agents")
+          .where("Email", isEqualTo: user?.email!)
+          .get();
 
       final String agentID = agentsQuery.docs.first.id;
-      await FirebaseFirestore.instance.collection('Parcels').doc(mostRecentDocument?.id).update({
+      await FirebaseFirestore.instance
+          .collection('Parcels')
+          .doc(mostRecentDocument?.id)
+          .update({
         'Agent-Id': agentID, // Add your new field here
       });
       print('Field added successfully');
@@ -110,123 +111,127 @@ class _MessageScreenState extends State<MessageScreen> {
         title: const Text('Pickup Details'),
         backgroundColor: tThemeMain,
         foregroundColor: Colors.white,
-
       ),
       body: Center(
         child: isLoading
             ? const CircularProgressIndicator(
-          color: tThemeMain,
-        )
-            : mostRecentDocument != null
-            ? Container(
-          height: 550,
-          width: 350,
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-              ),
-            ],
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.white,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Merchant Name: $merchantName',
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                'Merchant Phone: $merchantPhone',
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                'Pickup: $firmName',
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                'Pickup Address: $firmAddress',
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                'Parcel QTY: $parcelQTY',
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              Text(
-                'Item Description: $description',
-                textAlign: TextAlign.start,
-                style: const TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-              const SizedBox(
-                height: 205,
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: ()  {
-                    StatusController.instance.updateStatus(mostRecentDocument?.id,"Pickup initiated");
-                    updateFirestore();
-                    if (mostRecentDocument != null && mostRecentDocument!.data() != null) {
-                      Map<String, dynamic>? data = mostRecentDocument?.data() as Map<String, dynamic>?;
-                      if (data != null && data.containsKey('Agent-Id')) {
-                        isParcelAccepted = true;
-                      }
-                      if(isParcelAccepted){
-                        style: ElevatedButton.styleFrom(
-                          elevation: 5,
-                          shape: const RoundedRectangleBorder(),
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.white,
-                          side: const BorderSide(color: tThemeMain),
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 50),
-                        );
-                      }
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 5,
-                    shape: const RoundedRectangleBorder(),
-                    backgroundColor: tThemeMain,
-                    foregroundColor: Colors.white,
-                    side: const BorderSide(color: tThemeMain),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 50),
-                  ),
-                  child: const Text(
-                    'ACCEPT',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-
-                ),
+                color: tThemeMain,
               )
-            ],
-          ),
-        )
-            : const Text('No most recent document found or an error occurred.'),
+            : mostRecentDocument != null
+                ? Container(
+                    height: 550,
+                    width: 350,
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Merchant Name: $merchantName',
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'Merchant Phone: $merchantPhone',
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'Pickup: $firmName',
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'Pickup Address: $firmAddress',
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'Parcel QTY: $parcelQTY',
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'Item Description: $description',
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 205,
+                        ),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              StatusController.instance.updateStatus(
+                                  mostRecentDocument?.id, "Pickup initiated");
+                              updateFirestore();
+                              if (mostRecentDocument != null &&
+                                  mostRecentDocument!.data() != null) {
+                                Map<String, dynamic>? data = mostRecentDocument
+                                    ?.data() as Map<String, dynamic>?;
+                                if (data != null &&
+                                    data.containsKey('Agent-Id')) {
+                                  isParcelAccepted = true;
+                                }
+                                if (isParcelAccepted) {
+                                  style:
+                                  ElevatedButton.styleFrom(
+                                    elevation: 5,
+                                    shape: const RoundedRectangleBorder(),
+                                    backgroundColor: Colors.white,
+                                    foregroundColor: Colors.white,
+                                    side: const BorderSide(color: tThemeMain),
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 20, horizontal: 50),
+                                  );
+                                }
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              shape: const RoundedRectangleBorder(),
+                              backgroundColor: tThemeMain,
+                              foregroundColor: Colors.white,
+                              side: const BorderSide(color: tThemeMain),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 20, horizontal: 50),
+                            ),
+                            child: const Text(
+                              'ACCEPT',
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                : const Text(
+                    'No most recent document found or an error occurred.'),
       ),
     );
   }
